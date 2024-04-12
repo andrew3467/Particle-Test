4bdb0477bc0a6569c7151d3cb3006b26a11a0dee
andrew3467 <apgraser2@gmail.com>
2024-04-12 17:37:18 -0400

    Particle Test

---
+++ src/main/java/net/iirc/particletest/visualeffects/particle.java
@@ -0,0 +1,45 @@
+package net.iirc.particletest.visualeffects;
+
+
+import net.minecraft.client.*;
+import net.minecraft.client.player.*;
+import net.minecraft.world.level.*;
+import net.minecraft.world.phys.*;
+import net.minecraftforge.api.distmarker.*;
+import net.minecraftforge.event.*;
+import net.minecraftforge.eventbus.api.*;
+import net.minecraftforge.fml.common.*;
+import team.lodestar.lodestone.registry.common.particle.*;
+import team.lodestar.lodestone.systems.easing.*;
+import team.lodestar.lodestone.systems.particle.builder.*;
+import team.lodestar.lodestone.systems.particle.data.*;
+import team.lodestar.lodestone.systems.particle.data.color.*;
+import team.lodestar.lodestone.systems.particle.data.spin.*;
+
+import java.awt.Color;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class particle {
+    @SubscribeEvent
+    public static void clientTick(TickEvent.ClientTickEvent event) {
+        final LocalPlayer player = Minecraft.getInstance().player;
+        
+        if(player != null){
+            spawnParticles(player.level(), player.position());
+        }
+    }
+
+    private static void spawnParticles(Level level, Vec3 pos) {
+        Color startingColor = new Color(100, 0, 100);
+        Color endingColor = new Color(0, 100, 200);
+        WorldParticleBuilder.create(LodestoneParticleRegistry.WISP_PARTICLE)
+                .setScaleData(GenericParticleData.create(0.5f, 0).build())
+                .setTransparencyData(GenericParticleData.create(0.75f, 0.25f).build())
+                .setColorData(ColorParticleData.create(startingColor, endingColor).setCoefficient(1.4f).setEasing(Easing.BOUNCE_IN_OUT).build())
+                .setSpinData(SpinParticleData.create(0.2f, 0.4f).setSpinOffset((level.getGameTime() * 0.2f) % 6.28f).setEasing(Easing.QUARTIC_IN).build())
+                .setLifetime(40)
+                .addMotion(0, 0.01f, 0)
+                .enableNoClip()
+                .spawn(level, pos.x, pos.y, pos.z);
+    }
+}
